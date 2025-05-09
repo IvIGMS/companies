9768c37e19acddcf8391dd59c94b5eb90af560e4
ivanfrias <friasgilivan@gmail.com>
2025-05-09 15:05:15 +0200

    Adaptamos swagger para que pueda funcionar

--- src/main/java/com/ivanfrias/company/security/config/SecurityConfig.java
+++ src/main/java/com/ivanfrias/company/security/config/SecurityConfig.java
@@ -34,7 +34,12 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/auth/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**",
+                                "/v3/api-docs.yaml")
+                        .permitAll()
                         .anyRequest().authenticated()
                 )
                 .authenticationProvider(authenticationProvider())
